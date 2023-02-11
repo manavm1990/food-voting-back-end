27764708c0b994b2c5e9ff3e7b19b6b384e07f83
Manav Misra <manavm@visionwebsoft.com>
2023-02-11 11:07:05 -0600

    Add votelinks router to middleware

--- app/server.js
+++ app/server.js
@@ -3,6 +3,7 @@ import config from "./config.js";
 import cuisineRouter from "./cuisine/routes.js";
 import decodeUser from "./middleware/decode-user.js";
 import userRouter from "./user/routes.js";
+import voteLinkRouter from "./vote-link/routes.js";
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use("/api/cuisines", cuisineRouter);
 app.use(express.json());
 app.use(decodeUser);
 app.use("/api/users", userRouter);
+app.use("/api/votelinks", voteLinkRouter);
 
 export default () => {
   app.listen(config.port, () => {
